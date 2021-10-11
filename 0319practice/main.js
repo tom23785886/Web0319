0b086d6e5d1e5789bec6414ca79551deda14c895
tom23785886 <74181737+tom23785886@users.noreply.github.com>
2021-10-11 14:00:54 +0800

    Add files via upload

---
+++ 0319practice/main.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    var el=document.querySelector('h1');
+    el.style.backgroundColor='yellow';
+    el.style.border='0.25rem solid red';
+    el.textContent='Hello World';
+};
+
